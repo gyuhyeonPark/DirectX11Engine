2563e529f742de1ddde5401e0ac451c9330ba8eb
gyuhyeonPark <gmfrwh12@gmail.com>
2025-02-11 22:51:25 +0900

    .

---
+++ DirectX11Engine/Graphics.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <d3d11.h>
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib,"DirectXTK.lib")
+
+class Graphics
+{
+public:
+	bool Initialize(HWND hwnd, int width, int height);
+private:
+	bool InitializeDirectX(HWND hwnd, int width, int height);
+
+	ID3D11Device* device;
+	ID3D11DeviceContext* deviceContext;
+	IDXGISwapChain* swaphain;
+	ID3D11RenderTargetView* renderTargetView;
+
+};
+
